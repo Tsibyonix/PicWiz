b5cb3cc57efc6fe2be7238514e88b4565b57f444
Sanchit Samuel <sanchit.samuel@live.com>
2016-03-18 18:38:57 +0530

    * added app logo (temp) * added login and register framework

--- app/src/main/java/com/core/picwiz/LoginActivity.java
+++ app/src/main/java/com/core/picwiz/LoginActivity.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private boolean register = false;
 
+    private TextInputLayout mTextInputLayoutEmail;
+    private TextInputLayout mTextInputLayoutPassword;
     private TextInputLayout mTextInputLayoutUsername;
 
+    private EditText mEditTextEmail;
+    private EditText mEditTextPassword;
+    private EditText mEditTextUsername;
 
     private Button mButtonSignIn;
     private TextView mTextViewRegister;
@@ -21,11 +28,32 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mTextInputLayoutEmail = (TextInputLayout) findViewById(R.id.text_input_layout_email);
+        mTextInputLayoutPassword = (TextInputLayout) findViewById(R.id.text_input_layout_password);
         mTextInputLayoutUsername = (TextInputLayout) findViewById(R.id.text_input_layout_username);
+
+        mEditTextEmail = (EditText) findViewById(R.id.edit_text_email);
+        mEditTextPassword = (EditText) findViewById(R.id.edit_text_password);
+        mEditTextUsername = (EditText) findViewById(R.id.edit_text_username);
+
         mButtonSignIn = (Button) findViewById(R.id.button_sign_in);
         mTextViewRegister = (TextView) findViewById(R.id.text_view_register);
 
         mTextInputLayoutUsername.setVisibility(View.GONE);
+
+
+        mButtonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (register) {
+                    // proceed with register
+                    Toast.makeText(LoginActivity.this, "Registering", Toast.LENGTH_SHORT).show();
+                } else {
+                    // proceed with sign in
+                    Toast.makeText(LoginActivity.this, "Signing in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void onRegister(View view) {
