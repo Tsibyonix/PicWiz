a25658f204031764df39146a8cbe3074bc9e752a
Sanchit Samuel <sanchit.samuel@live.com>
2016-03-18 16:09:06 +0530

    * basic login layout file * register or sign in switch

--- app/src/main/java/com/core/picwiz/LoginActivity.java
+++ app/src/main/java/com/core/picwiz/LoginActivity.java
@@ -1,13 +1,44 @@
 package com.core.picwiz;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private boolean register = false;
+
+    private TextInputLayout mTextInputLayoutUsername;
+
+
+    private Button mButtonSignIn;
+    private TextView mTextViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mTextInputLayoutUsername = (TextInputLayout) findViewById(R.id.text_input_layout_username);
+        mButtonSignIn = (Button) findViewById(R.id.button_sign_in);
+        mTextViewRegister = (TextView) findViewById(R.id.text_view_register);
+
+        mTextInputLayoutUsername.setVisibility(View.GONE);
+    }
+
+    public void onRegister(View view) {
+        if (register) {
+            mTextInputLayoutUsername.setVisibility(View.GONE);
+            mButtonSignIn.setText(R.string.action_sign_in);
+            mTextViewRegister.setText(R.string.prompt_or_register);
+            register = false;
+        } else {
+            mTextInputLayoutUsername.setVisibility(View.VISIBLE);
+            mButtonSignIn.setText(R.string.action_register);
+            mTextViewRegister.setText(R.string.prompt_or_sign_in);
+            register = true;
+        }
     }
 }
